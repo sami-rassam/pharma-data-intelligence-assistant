ee5058bd9d8cc24175988e5cb64406168037a431
sami-rassam <samirassam96@gmail.com>
2026-06-04 08:53:42 +0100

    Fix llm.py syntax

--- src/llm.py
+++ src/llm.py
@@ -28,4 +28,4 @@ def get_llm(temperature: float = 0.2):
         groq_api_key=groq_api_key,
         model="llama-3.1-8b-instant",
         temperature=0.1
-    )docker build -t pharma-data-intelligence-assistant .
+    )
